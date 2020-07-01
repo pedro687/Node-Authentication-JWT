89e497234a70ac576479b2ab228dddbc7935a860
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-06-30 22:12:42 -0300

    Configurando o Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: '3307',
+    username: 'root',
+    password: '1234567',
+    database: 'teste_sequelize',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+
+}
